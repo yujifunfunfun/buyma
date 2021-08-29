07c85e81b5764efa992aba52592d1e5a5c2b06c2
yujifunfunfun <s2vuw4bfq44uebt19qx8@docomo.ne.jp>
2021-08-29 22:56:01 +0900

    2

--- edit_date.py
+++ edit_date.py
@@ -2,12 +2,24 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import eel
 import time
 
 def main(date):
 
+    option = Options()                         
+    option.add_argument('--headless') 
+    option.add_argument('--lang=ja-JP')
+    option.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+    option.add_argument('--ignore-certificate-errors')
+    option.add_argument('--ignore-ssl-errors')
+    option.add_argument('--incognito') 
+
+
     #ここで、バージョンなどのチェックをする
-    driver = webdriver.Chrome(ChromeDriverManager().install())   
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=option)   
+
 
     # Webサイトを開く
     driver.get("https://www.buyma.com/login/")
@@ -17,24 +29,31 @@ def main(date):
     driver.find_element_by_id("txtLoginId").send_keys('tyuji051605160516@gmail.com')
     # パスワード入力
     driver.find_element_by_id("txtLoginPass").send_keys('tozuka1998')
-    # 検索ボタンクリック
+    # ログインボタンクリック
     driver.find_element_by_id("login_do").click()
+    eel.view_log_js('ログインしました')
     time.sleep(5)
     # 出品画面へ遷移
-    driver.find_element_by_xpath('//*[@id="wrapper"]/div[5]/div[2]/div/div[2]/ul/li[6]/a').click()
+    driver.get("https://www.buyma.com/my/sell/?tab=b/")
+    eel.view_log_js('出品管理へ遷移')
+    
     time.sleep(3)
 
+    eel.view_log_js('処理が開始されました')
     # 日付変更動作
     edit_buttons = driver.find_elements_by_class_name('_item_edit_yukodate')
     input_lists = driver.find_elements_by_class_name("_item_yukodate_edit")
-
-    for edit_button,input_list in zip(edit_buttons,input_lists):
+    edit_buttons_num = len(edit_buttons)
+    for count,(edit_button,input_list) in enumerate(zip(edit_buttons,input_lists),1):
         edit_button.click()
         time.sleep(2)
         input_list.clear()
         time.sleep(1)
         input_list.send_keys(date)
         time.sleep(1)
+        eel.view_log_js(f"{count}/{edit_buttons_num}完了")
+
+    eel.view_log_js("処理が完了しました")
     
         
         

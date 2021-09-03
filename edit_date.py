5deef5802b21bea7ed0dfa6bbd7f724368d9a1f7
Tozuka Yuji <yuji_tozuka@ydktechs.co.jp>
2021-09-03 19:22:47 +0900

    8

--- edit_date.py
+++ edit_date.py
@@ -10,7 +10,6 @@ import csv
 import re
 import datetime
 
-
 logger = set_logger(__name__)
 
 def start_chrome():
@@ -22,7 +21,7 @@ def start_chrome():
     # option.add_argument('--ignore-certificate-errors')
     # option.add_argument('--ignore-ssl-errors')
     # option.add_argument('--incognito') 
-    option.add_argument("window-size=1500,1000")
+    option.add_argument("window-size=1300,1000")
     #ここで、バージョンなどのチェックをする
     global driver
     driver = webdriver.Chrome(ChromeDriverManager().install(),options=option)   
@@ -48,30 +47,32 @@ def login():
         driver.find_element_by_id("login_do").click()
         logger.info('ログインしました')
         eel.view_log_js('ログインしました')
-        time.sleep(3)
+        time.sleep(8)
     except Exception as e:
         logger.info(e)
         eel.view_log_js('エラーが発生しました')
-        driver.quit()
 
-def cal_click_count(input_month):
-    date = str(datetime.date.today())
-    p = r'-(.*)-'
-    this_month = re.search(p, date).group(1)
-    this_month = this_month.lstrip('0')
-    click_count = int(input_month) - int(this_month)
-    return click_count
-
-def operate_calendar(click_count,input_day):
+def operate_calender(input_month,input_day):
     edit_date_buttons = driver.find_elements_by_class_name('_item_edit_yukodate')
+
     edit_date_buttons_num = len(edit_date_buttons)
     for count,edit_date_button in enumerate(edit_date_buttons,1):
         edit_date_button.click()
         time.sleep(1)
+
+        date = driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/span[2]').text
+        p = r'年 年 (.*)月'
+        q = r'年 (.*)月' 
+        try: 
+            re.search(p, date).group(1)
+            due_month = re.search(p, date).group(1)
+        except:
+            due_month = re.search(q, date).group(1)
+
+        click_count = int(input_month) - int(due_month) 
         for i in range(0,int(click_count)):
             driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/a[2]/span').click()
             time.sleep(1)
-        
         driver.find_element_by_xpath(f'//a[contains(@href,"#") and text()={input_day}]').click()
         time.sleep(1)
         logger.info(f"{count}/{edit_date_buttons_num}完了")
@@ -80,30 +81,29 @@ def operate_calendar(click_count,input_day):
 
 def edit_date(input_month,input_day):
     try:
-        # カレンダーの「次」ボタンを押す回数を計算
-        click_count = cal_click_count(input_month)
         # 出品画面へ遷移
-        driver.get("https://www.buyma.com/my/sell/?tab=b/")
-        logger.info('出品管理へ遷移しました')
-        eel.view_log_js('出品管理へ遷移しました')
+        driver.get("https://www.buyma.com/my/sell/?status=for_sale&tab=b#/")
+        logger.info('リクエスト受付中ページへ遷移しました')
+        eel.view_log_js('リクエスト受付中ページへ遷移しました')
         time.sleep(3)
         # 表示件数の変更
         driver.find_element_by_class_name("js-row-count-options").click()
         driver.find_element_by_xpath('//*[@id="row-count-options"]/option[3]').click()
         time.sleep(2)
-        # 日付変更動作
+        # 日付変更処理
         logger.info('日付変更処理が開始されました')
         eel.view_log_js('日付変更処理が開始されました')
-        operate_calendar(click_count,input_day)
+        operate_calender(input_month,input_day)
 
         while len(driver.find_elements_by_xpath('//a[@rel="next" and text()="次へ"]')) > 0:
             driver.find_element_by_xpath('//a[@rel="next" and text()="次へ"]').click()
             logger.info('次のページに遷移しました')
             eel.view_log_js("次のページに遷移しました")
-            operate_calendar(click_count,input_day)
-           
-        logger.info('処理が完了しました')
-        eel.view_log_js("処理が完了しました")
+            # 表示件数の変更
+            driver.find_element_by_class_name("js-row-count-options").click()
+            driver.find_element_by_xpath('//*[@id="row-count-options"]/option[3]').click()
+            # 日付変更処理
+            operate_calender(input_month,input_day)
         driver.quit()
     except Exception as e:
         logger.info(e)
--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import re
 
 
 date = str(datetime.date.today())
-date = '2021-10-9'
-p = r'-(.*)-'
+date = '年 年 9月'
+p = r'年 年 (.*)月'
 this_month = re.search(p, date).group(1)
 this_month = this_month.strip('0')
 print(this_month)
+print('a')
